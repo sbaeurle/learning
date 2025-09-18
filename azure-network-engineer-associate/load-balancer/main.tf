f33e2904555a61025eab639456e1d605ca9872cf
Simon Bäurle <simonbaeurle@outlook.de>
2025-09-18 10:35:19 +0200

    add test vm

--- azure-network-engineer-associate/load-balancer/main.tf
+++ azure-network-engineer-associate/load-balancer/main.tf
@@ -84,3 +84,55 @@ resource "azurerm_subnet" "frontend-subnet" {
 
   address_prefixes = ["10.1.2.0/24"]
 }
+
+resource "azurerm_network_interface" "test-vm-nic" {
+  name                = "test-vm-nic"
+  resource_group_name = azurerm_resource_group.lb-group.name
+  location            = azurerm_resource_group.lb-group.location
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.backend-subnet.id
+    private_ip_address_allocation = "Dynamic"
+    primary                       = true
+  }
+
+  tags = azurerm_resource_group.lb-group.tags
+}
+
+resource "azurerm_network_interface_security_group_association" "test-vm-nsg-asc" {
+  network_interface_id      = azurerm_network_interface.test-vm-nic.id
+  network_security_group_id = azurerm_network_security_group.backend-nsg.id
+}
+
+# Backend Virtual Machines
+resource "azurerm_windows_virtual_machine" "test-vm" {
+  name                = "test-vm"
+  resource_group_name = azurerm_resource_group.lb-group.name
+  location            = azurerm_resource_group.lb-group.location
+  size                = var.vm-size
+  admin_username      = var.admin-username
+  admin_password      = var.admin-password
+
+  network_interface_ids = [
+    azurerm_network_interface.test-vm-nic.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = azurerm_resource_group.lb-group.tags
+}
